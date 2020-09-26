21ef6daeda5666ce36c032acdcf71ecaf2a1797b
Tolkata99 <Tolkata_04@abv.bg>
2020-09-26 18:22:14 +0300

    exerrsize

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            double waterTank = 255;
+            double waterCallect = 0;
+            for (int i = 0; i < n; i++)
+            {
+                double water = double.Parse(Console.ReadLine());
+                
+                if (waterTank - water >= 0)
+                {
+                    waterTank -= water;
+                    waterCallect += water;
+
+                }
+                else
+                {
+                    Console.WriteLine("Insufficient capacity!");
+                }
+            }
+            Console.WriteLine(waterCallect);
+        }
+    }
+}
